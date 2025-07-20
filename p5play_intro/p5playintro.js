facf1ffed5f36cc0caa83ea83849eef3ef4361a6
JamalChaosGalaxy <nicholaswg1@gmail.com>
2025-07-20 03:25:52 +0000

    Jul 20, 2025, 3:25 AM

--- p5play_intro/p5playintro.js
+++ p5play_intro/p5playintro.js
@@ -3,9 +3,9 @@ function setup() {
   // Set up the canvas
   new Canvas(800, 400);
   background(250); //background color
-  ball = new Sprite();
+  // ball = new Sprite();
   // Basic shape testing
-  
+
   // write your codes here
  
   // End Basic shape testing
