d8bf19c91efabdeb9fc5668b9b62796192542926
JamalChaosGalaxy <nicholaswg1@gmail.com>
2025-07-20 03:43:16 +0000

    Jul 20, 2025, 3:43 AM

--- p5play_intro/p5playintro.js
+++ p5play_intro/p5playintro.js
@@ -7,7 +7,7 @@ function setup() {
   // Basic shape testing
   fill('skyblue');//fill the color
   stroke('pink');// create the border color
-  strokeweight(10);// border thickness
+
   // write your codes here
   circle(30, 30, 50);// circle (x, y, diameter)
   rect(50, 50, 100, 200);// rectangle (x, y, width, height)
