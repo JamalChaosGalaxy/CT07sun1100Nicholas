0e0a6e0a10e9a86f88183497fc21f89a3e38f59a
JamalChaosGalaxy <nicholaswg1@gmail.com>
2025-07-20 03:39:23 +0000

    Jul 20, 2025, 3:39 AM

--- p5play_intro/p5playintro.js
+++ p5play_intro/p5playintro.js
@@ -11,7 +11,8 @@ function setup() {
   rect(50, 50, 100, 200);// rectangle (x, y, width, height)
   circle(760, 30, 50);
   circle(760, 370, 50);
-  fill("skyblue");
+  fill("skyblue");//fill the color
+  fill()
   // End Basic shape testing
 
    // Create a bouncing ball sprite
