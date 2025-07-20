7999af74d97344a160751054fedbcf0b16757b0d
JamalChaosGalaxy <nicholaswg1@gmail.com>
2025-07-20 04:08:54 +0000

    Jul 20, 2025, 4:08 AM

--- p5play_intro/p5playintro.js
+++ p5play_intro/p5playintro.js
@@ -47,5 +47,5 @@ function draw() {
   fill(0);
   textsize(10);
   text("Ball: ("+ int()(ball.x)+","+ int(ball.y)+")", 10,20); 
-  text("Mouse: ("+MouseX + "," +MouseY + ")", 10,40 ); 
+  text("Mouse: ("+mouseX + "," +mouseY + ")", 10,40 ); 
 }
