0bc116c6e2741041f5a92398bac2a39f2446b5e3
JamalChaosGalaxy <nicholaswg1@gmail.com>
2025-07-20 03:19:13 +0000

    Jul 20, 2025, 3:19 AM

--- p5play_intro/p5playintro.js
+++ p5play_intro/p5playintro.js
@@ -3,7 +3,7 @@ function setup() {
   // Set up the canvas
   new Canvas(800, 400);
   background(250); //background color
-  ball = 
+  ball = new SpeechRecognitionResult
   // Basic shape testing
   // write your codes here
  
