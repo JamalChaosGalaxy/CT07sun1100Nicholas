b86e3388a19f479a71520af0c06365e800489e6a
JamalChaosGalaxy <nicholaswg1@gmail.com>
2025-07-27 03:27:43 +0000

    Jul 27, 2025, 3:27 AM

--- p5play_intro/basic.js
+++ p5play_intro/basic.js
@@ -22,7 +22,8 @@ function setup() {
   // --- Exercise: Sum of first 10 even numbers ---
   // write your codes here
   let base = 10;
-  let
+  let heihgt = 5;
+  
   // --- Exercise: Age category classification ---
   // write your codes here
 
