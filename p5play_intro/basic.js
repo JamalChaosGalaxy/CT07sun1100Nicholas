5e988bb463927547bf30b09fd6a869bc5f3b989e
JamalChaosGalaxy <nicholaswg1@gmail.com>
2025-07-27 03:27:08 +0000

    Jul 27, 2025, 3:27 AM

--- p5play_intro/basic.js
+++ p5play_intro/basic.js
@@ -5,17 +5,17 @@ function setup() {
   textSize(25);
   fill(0);
   // write your codes here
-  let a = 5;
-  let b = 10;
+  // let a = 5;
+  // let b = 10;
 
-  let sum = a + b;
-  let product = a * b;
+  // let sum = a + b;
+  // let product = a * b;
 
-  console.log("Sum of a and b is:", sum);
-  console.log("Product of a and b is:", product);
-  // This lien of code is to display text on the canvas("value",x,y)
-  text("Sum of a and b is:"+sum,20,30);
-  text("Product of a and b is:"+product,40,50);
+  // console.log("Sum of a and b is:", sum);
+  // console.log("Product of a and b is:", product);
+  // // This lien of code is to display text on the canvas("value",x,y)
+  // text("Sum of a and b is:"+sum,20,30);
+  // text("Product of a and b is:"+product,30,50);
   // --- Exercise: Area of Triangle ---
   // write your codes here
 
