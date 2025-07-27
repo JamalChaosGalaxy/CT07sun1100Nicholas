69444fd8d69b8471a587e45ef01435e940bd409a
JamalChaosGalaxy <nicholaswg1@gmail.com>
2025-07-27 03:56:01 +0000

    Jul 27, 2025, 3:56 AM

--- p5play_intro/basic.js
+++ p5play_intro/basic.js
@@ -55,8 +55,9 @@ function setup() {
   // for (let i = 20; i>=1;i--){
   //   console.log(i)
   // }
-  for (let i = 3; i<=46;i + 3){
-    console.log(i)
-  }
+  // for (let i = 3; i<=46;i + 3){
+  //   console.log(i)
+  // }
+  for (let i = 2;i<=10; i + 2)
 }
 
