30d3fe966ee67d5438f1a72613c7e0e8febb46fc
JamalChaosGalaxy <nicholaswg1@gmail.com>
2025-08-10 03:37:53 +0000

    Aug 10, 2025, 3:37 AM

--- flappybird/flappybird01.js
+++ flappybird/flappybird01.js
@@ -14,8 +14,32 @@ function setup(){
     bird.width = 30;
     bird.height = 30;
     bird.img = flappyMidImg;
+
+    bird.collider = "dynamic";
+    bird.mass = 2;
+    bird.drag = 0.02;
+    bird.bounciness = 0.5;
+    world.gravity.y = 10;
+
+    floor = new Sprite();
+    floor.x = 200;
+    floor.y = height - 20;
+    floor.width = 400;
+    floor.height = 125;
+    floor.collider = "static";
+    floor.img = base;
 }
 
 function draw(){
     image(bg, 0, 0, width, height);
+    if(kb.presses('space')){
+        bird.vel.y = -5;
+        bird.sleeping = false;
+    }
+    //debugging  info
+    fill("blue");
+    textSize(14);
+    text("Vel.y" + bird.vel.y.toFixed(2), 10,20);
+    text("is Moving" + bird.isMoving, 10,40)
+    text("Sleeping" + bird.sleeping, 10,60)
 }
