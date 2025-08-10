e6c38a194cf89683b2be5f5b80b856cfb8685106
JamalChaosGalaxy <nicholaswg1@gmail.com>
2025-08-10 04:39:29 +0000

    Aug 10, 2025, 4:39 AM

--- flappybird/playground/pg04_setupsprite.js
+++ flappybird/playground/pg04_setupsprite.js
@@ -1,10 +1,12 @@
-let bird, floor;
-let bg, base, flappyMidImg;
+let bird, floor;// declare objects variable
+let bg, base, flapMidImg;// declar the images variables;
 function preload(){
-    bg = loadImage('assets/background-day.png');
-    base = loadImage('assets/base.png');
-    flappyMidImg = loadImage('assets/yellowbird-midflap.png');
+ //load the base the bg and the fpappy bird   
+ bg = loadImage('assets/background-day.png');
+ base = loadImage('assets/base.png');
+ flapMidImg = loadImage('assets/yellowbird-midflap.png');
 }
+
 function setup(){
     new Canvas(400, 600);
 
@@ -12,8 +14,8 @@ function setup(){
     bird.x = width/2;
     bird.y = 200;
     bird.width = 30;
-    bird.height = 30;
-    bird.img = flappyMidImg;
+    bird.height =30;
+    bird.img = flapMidImg; 
 
     bird.collider = "dynamic";
     bird.mass = 2;
@@ -25,21 +27,25 @@ function setup(){
     floor.x = 200;
     floor.y = height - 20;
     floor.width = 400;
-    floor.height = 125;
+    floor.height =125;
     floor.collider = "static";
-    floor.img = base;
+    floor.img = base; 
 }
 
 function draw(){
-    image(bg, 0, 0, width, height);
+    //draw the bg
+    image(bg,0,0, width, height); 
+    
     if(kb.presses('space')){
         bird.vel.y = -5;
         bird.sleeping = false;
     }
-    //debugging  info
+
+    //debuggin info
     fill("blue");
     textSize(14);
     text("Vel.y" + bird.vel.y.toFixed(2), 10,20);
-    text("is Moving" + bird.isMoving, 10,40)
-    text("Sleeping" + bird.sleeping, 10,60)
+    text("is Moving: " + bird.isMoving, 10,40);
+    text("Sleeping: " + bird.Sleeping, 10,60);
+
 }
