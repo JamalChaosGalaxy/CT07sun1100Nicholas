e1e8fe62a6145256bed93c411cab3278af2283c7
JamalChaosGalaxy <nicholaswg1@gmail.com>
2025-08-10 04:40:13 +0000

    Aug 10, 2025, 4:40 AM

--- flappybird/playground/pg05b_spriteobstacles.js
+++ flappybird/playground/pg05b_spriteobstacles.js
@@ -1,17 +1,19 @@
-let bird, floor;
-let bg, base, flappyMidImg;
-let flapupImg, flapdownImg;
-let pipeGroup;
+let bird, floor;// declare objects variable
+let bg, base, flapMidImg;// declar the images variables;
+let flagUpImg, flapDownImg;
+let pipeGroup; // declare var for grp;
 let pipe;
-let topPipe, bottomePipe;
+let topPipe, bottomPipe;
 function preload(){
-    bg = loadImage('assets/background-day.png');
-    base = loadImage('assets/base.png');
-    flappyMidImg = loadImage('assets/yellowbird-midflap.png');
-    flappyupImg = loadImage('assets/yellowbird-upflap.png');
-    flappydownImg = loadImage('assets/yellowbird-downflap.png');
-    pipe = loadImage('assets/pipe-green.png');// loads the pipe image
+ //load the base the bg and the fpappy bird   
+ bg = loadImage('assets/background-day.png');
+ base = loadImage('assets/base.png');
+ flapMidImg = loadImage('assets/yellowbird-midflap.png');
+ flapDownImg = loadImage('assets/yellowbird-upflap.png');
+ flagUpImg = loadImage('assets/yellowbird-downflap.png');
+ pipe = loadImage('assets/pipe-green.png'); // preloads the image for pipe
 }
+
 function setup(){
     new Canvas(400, 600);
 
@@ -19,8 +21,8 @@ function setup(){
     bird.x = width/2;
     bird.y = 200;
     bird.width = 30;
-    bird.height = 30;
-    bird.img = flappyMidImg;
+    bird.height =30;
+    bird.img = flapMidImg; 
 
     bird.collider = "dynamic";
     bird.mass = 2;
@@ -32,54 +34,61 @@ function setup(){
     floor.x = 200;
     floor.y = height - 20;
     floor.width = 400;
-    floor.height = 125;
+    floor.height =125;
     floor.collider = "static";
-    floor.img = base;
+    floor.img = base; 
 
-    pipeGroup = new Sprite();
+    pipeGroup = new Group();
 }
 
 function draw(){
-    image(bg, 0, 0, width, height);
+    //draw the bg
+    image(bg,0,0, width, height); 
+    
     if(kb.presses('space')){
         bird.vel.y = -5;
         bird.sleeping = false;
     }
-
+    
     if(bird.vel.y < -1){
-        bird.img = flapupImg;
+        bird.img = flagUpImg;
         bird.rotation = -30;
-    }else if(bird.vel.y > 1){
-        bird.img = flapdownImg;
+    }
+    else if(bird.vel.y > 1){
+        bird.img = flapDownImg;
         bird.rotation = 30;
-    }else{
-        bird.img = flappyMidImg;
-        bird.rotation = 0;
     }
-
-    if(frameC  ount === 1){
-        spamPipePair()// calling the function spampipe pair which we create below
+    else{
+        bird.img = flapMidImg;
+        bird.rotation =0;
     }
-    //debugging  info
+    
+    if(frameCount === 1){
+        spawnPipePair()// calling the function spawnpipe pair which we crete be;ow
+    }
+    //debuggin info
     fill("blue");
     textSize(14);
     text("Vel.y" + bird.vel.y.toFixed(2), 10,20);
-    text("is Moving" + bird.isMoving, 10,40)
-    text("Sleeping" + bird.sleeping, 10,60)
+    text("is Moving: " + bird.isMoving, 10,40);
+    text("Sleeping: " + bird.Sleeping, 10,60);
+
 }
 
-function spamPipePair(){
-    let gap = 50;
-    let midY = height/2;
-    //create top pipe Sprite
-    topPipe = newSprite(400,midY-gap/2 - 200, 52,320, 'static');
-    bottomPipe.img = pipe
+function spawnPipePair(){
 
-    //create bottom pipe Sprite
-    bottomPipe = newSprite(400,midY+gap/2 - 200, 52,320, 'static');
+    //control the gap and height of the top and bottom pipe
+    let gap = 50;
+    let midY = height /2;
+    //create top pipe sprite
+    topPipe = new Sprite(400,midY-gap /2 -200,52,320,'static');
+    topPipe.img = pipe;
+
+    //create bottom pipe sprite
+    bottomPipe = new Sprite(400,midY+gap /2 +200,52,320,'static');
     bottomPipe.img = pipe;
 
     pipeGroup.add(bottomPipe);
-    pipeGroup.add(topPipe)
+    pipeGroup.add(topPipe);
     pipeGroup.layer = 0;
 }
