d5120a9aa0be374c4276bf403a1e01f0043a35d0
JamalChaosGalaxy <nicholaswg1@gmail.com>
2025-08-17 03:44:57 +0000

    Aug 17, 2025, 3:44 AM

--- flappybird/playground/pg06_collision.js
+++ flappybird/playground/pg06_collision.js
@@ -12,7 +12,7 @@ function preload(){
  flapDownImg = loadImage('assets/yellowbird-upflap.png');
  flagUpImg = loadImage('assets/yellowbird-downflap.png');
  pipe = loadImage('assets/pipe-green.png'); // preloads the image for pipe
- 
+ gaemoverImg = loadImage('assets/gameover.png')
 }
 
 function setup(){
