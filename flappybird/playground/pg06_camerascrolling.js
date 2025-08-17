c858866159eb1a3f8c974a2b0517c4affcccfbb3
JamalChaosGalaxy <nicholaswg1@gmail.com>
2025-08-17 03:32:33 +0000

    Aug 17, 2025, 3:32 AM

--- flappybird/playground/pg06_camerascrolling.js
+++ flappybird/playground/pg06_camerascrolling.js
@@ -93,7 +93,7 @@ function spawnPipePair(){
 
     //control the gap and height of the top and bottom pipe
     let gap = 50;
-    let midY = random;
+    let midY = random(250, height-250);
     //create top pipe sprite
     topPipe = new Sprite(bird.x+400,midY-gap /2 -200,52,320,'static');//prev x cooridnates for pipe is at a fixed 400 to make it 
     topPipe.img = pipe;                                             // appear infront of bird we do bird.x + 400
