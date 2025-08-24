725543b9b4fdd489ca21c7b53596d8a0c07f8e27
JamalChaosGalaxy <nicholaswg1@gmail.com>
2025-08-24 03:57:30 +0000

    Aug 24, 2025, 3:57 AM

--- flappybird/playground/pg07_scoring.js
+++ flappybird/playground/pg07_scoring.js
@@ -163,5 +163,6 @@ function drawScore(x, y, score, digitWidth, digitHeight){
         let digit = int(scoreStr[i]);
         //find the x positions of the digit
         let xPos = startX + i * digitWidth;
+        let digitSprite = new scoreDigits
     }
 }
