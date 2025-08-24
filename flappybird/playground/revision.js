770ec78c8e7aef97eec8bbf179f117d62e1522f2
JamalChaosGalaxy <nicholaswg1@gmail.com>
2025-08-24 04:09:31 +0000

    Aug 24, 2025, 4:09 AM

--- flappybird/playground/revision.js
+++ flappybird/playground/revision.js
@@ -1,9 +1,10 @@
 //create the two required must have functions
-let ball//declare var balll
+let ball;//declare var balll
 function setup(){
     //create canvas 800 x 800
 
-    new Canvas(800, 800)
+    new Canvas(800, 800);
+    
 }
 
 function draw(){
