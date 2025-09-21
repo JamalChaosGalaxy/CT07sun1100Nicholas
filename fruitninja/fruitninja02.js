de491503a1084bb872d9450504e83ac058c2e154
JamalChaosGalaxy <nicholaswg1@gmail.com>
2025-09-21 04:14:00 +0000

    Sep 21, 2025, 4:14 AM

--- fruitninja/fruitninja02.js
+++ fruitninja/fruitninja02.js
@@ -36,7 +36,7 @@ function spawnFruit(){
     let fruit = new fruitGroup.Sprite(randomX, height+ 20, 40);
     fruit.image = fruitData.whole;
     fruit.type = fruitData;
-    fruit.vel.y
+    fruit.vel.y = 
     fruit.vel.x
-    fr
+    fruit.friction = 0;
 }
