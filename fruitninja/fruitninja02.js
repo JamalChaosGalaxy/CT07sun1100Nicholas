da2642bc1097296065be389d3183347d500baaf7
JamalChaosGalaxy <nicholaswg1@gmail.com>
2025-09-21 03:59:39 +0000

    Sep 21, 2025, 3:59 AM

---
+++ fruitninja/fruitninja02.js
@@ -0,0 +1,16 @@
+let dojoBG;
+
+function preload(){
+    dojoBG = loadImage('assets/dojobackground.png');
+}
+
+function setup(){
+    new Canvas(800, 600);
+    // set gravity to 10;
+    world.gravity.y = 10;
+}
+
+function draw(){
+    clear();
+    image(dojoBG, 0, 0, width, height);
+}
