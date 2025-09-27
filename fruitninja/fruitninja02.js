17fc2d08f482ce8b70690a03e6b26df0cad98a6a
JamalChaosGalaxy <nicholaswg1@gmail.com>
2025-09-27 08:40:12 +0000

    Sep 27, 2025, 8:40 AM

--- fruitninja/fruitninja02.js
+++ fruitninja/fruitninja02.js
@@ -33,7 +33,8 @@ function draw(){
     }
     // handles my slicicng effect
     if(mouse.pressing()){
-        trall = new Sprite(mouse.x, mouse.y, 7)
+        trall = new Sprite(mouse.x, mouse.y, 7);
+        trall.collider = "none"
     }
 }
 
