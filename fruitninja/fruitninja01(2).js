b9d4f83dc0e7ea2f6ca159aeb375a70c93a9ab19
JamalChaosGalaxy <nicholaswg1@gmail.com>
2025-09-27 08:47:08 +0000

    Sep 27, 2025, 8:47 AM

---
+++ fruitninja/fruitninja01(2).js
@@ -0,0 +1,62 @@
+let fruitGroup;
+let fruitTypes = [];
+let dojoBG;
+
+function preload(){
+    dojoBG = loadImage('assets/dojobackground.png');
+    let peach = {
+        whole: loadImage('assets/peachwhole.png')
+    };
+    let watermelon = {
+        whole: loadImage('assets/watermelonwhole.png')
+    }; 
+    fruitTypes = [peach, watermelon];
+   
+}
+
+function setup(){
+    new Canvas(800, 600);
+    // set gravity to 10;
+    world.gravity.y = 10;
+
+    fruitGroup = new Group();
+
+    
+    // fruittype = [];
+}
+
+function draw(){
+    clear();
+    image(dojoBG, 0, 0, width, height);
+    if (frameCount % 60 === 0){// % refers to the remainder so if framecount divided by 120, if the remainder is 0, theb I will call spawnfruit function
+        spawnFruit();
+    }
+    // handles my slicicng effect
+    if(mouse.pressing()){
+        trail = new Sprite(mouse.x, mouse.y, 10);
+        trail.collider = 'none';
+        trail.color = "red";
+        trail.life = 10;
+    }
+}
+
+function spawnFruit(){
+    // fruitTypes = [peach, watermelon];
+    // let peach = {
+    //     whole:loadImage('assets/peachwhole.png')
+    // };
+    //tell the program to randomly pick one fruit object
+    let fruitData = random(fruitTypes);
+    let randomX = random(300, 500);
+    // create a fruit and stroing it in the group
+    let fruit = new fruitGroup.Sprite(randomX, height+ 20, 40);
+    //basically loading the image adnt eh image is sored in the array and the key to the image values
+    //is whole hence its fruitData.whole (arrayname.whichkey)
+    fruit.image = fruitData.whole;
+    //telling the program that it is going to be a random fruit form the array
+    fruit.type = fruitData;
+    //setting the game physics the numbers can play around
+    fruit.vel.y = random(-10, - 14);
+    fruit.vel.x = random(-2, 2);
+    fruit.friction = 0;
+}
