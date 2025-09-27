4ebd0443bb4a7bfe2c5ad7dac98b19a3bd9dd8f5
JamalChaosGalaxy <nicholaswg1@gmail.com>
2025-09-27 09:12:47 +0000

    Sep 27, 2025, 9:12 AM

--- fruitninja/fruitninja02.js
+++ fruitninja/fruitninja02.js
@@ -91,7 +91,7 @@ function splitFruit(x, y, fruitData){
     let left = new fruitHalves.Sprite(x-10, y, 40, 40);
     left.img = fruitData.half1;
     left.vel.x = -3;
-    left.vel.y = random(-5, -2);
+    left.vel.y = random(-10, -2);
     left.rotationSpeed = -5;
 
 
