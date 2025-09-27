ea2740fc24f29536dddd9a64e40eff22f861f4ff
JamalChaosGalaxy <nicholaswg1@gmail.com>
2025-09-27 08:29:56 +0000

    Sep 27, 2025, 8:29 AM

--- fruitninja/fruitninja02.js
+++ fruitninja/fruitninja02.js
@@ -5,7 +5,7 @@ let dojoBG;
 function preload(){
     dojoBG = loadImage('assets/dojobackground.png');
     let peach = {
-        whole: loadImage('assets/peachwhole.png')
+        whole: loadImage('fruitninja/assets/peachwhole.png')
     };
     let watermelon = {
         whole: loadImage('assets/watermelonwhole/png')
