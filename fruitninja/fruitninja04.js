d51a1feaa5c0c552b2a6e801b2a4089af73714f2
JamalChaosGalaxy <nicholaswg1@gmail.com>
2025-10-12 03:26:19 +0000

    Oct 12, 2025, 3:26 AM

--- fruitninja/fruitninja04.js
+++ fruitninja/fruitninja04.js
@@ -37,7 +37,7 @@ function draw(){
 
     if(gameState === 'start'){
         fill(0, 100);
-        
+        rect(0, )
     }
     if (frameCount % 60 === 0){// % refers to the remainder so if framecount divided by 120, if the remainder is 0, theb I will call spawnfruit function
         spawnFruit();
