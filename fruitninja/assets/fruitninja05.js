9522272fa5e802d743248418f29d126ae5990109
JamalChaosGalaxy <nicholaswg1@gmail.com>
2025-10-12 04:23:58 +0000

    Oct 12, 2025, 4:23 AM

--- fruitninja/assets/fruitninja05.js
+++ fruitninja/assets/fruitninja05.js
@@ -1,97 +1,98 @@
 let fruitGroup;
-let fruitTypes = [];
-let dojoBG;
+let fruitTypes =[];
+let dogoBG;
 let fruitHalves;
-let score = 0;
-let MissedFruits = 0;
-let gameState = 'start';
+let score=0;
+let missFruits =0;
+let gameState= 'start';
 let sliceSound;
 let backgroundTrack;
-
 function preload(){
-    dojoBG = loadImage('assets/dojobackground.png');
+     dogoBG = loadImage('assets/dojobackground.png');
+    //declare peach 
     let peach = {
-        whole: loadImage('assets/peachwhole.png'),
-        half1: loadImage('assets/peachhalf.png')
+        whole:loadImage('assets/peachwhole.png'),
+        half:loadImage('assets/peachhalf.png')
     };
     let watermelon = {
-        whole: loadImage('assets/watermelonwhole.png'),
-        half1: loadImage('assets/watermelonhalf.png')
-    }; 
-    //store each fruit object into fruit Types array[]
-    fruitTypes = [peach, watermelon];
-    sliceSound = loadSound("assets/fruit-ninja-combo.mp3");
-    backgroundTrack = loadSound("assets/fruit-ninja-bgtrack.mp3");
-
-   
+        whole:loadImage('assets/watermelonwhole.png'),
+        half: loadImage('assets/watermelonhalf.png')
+    }
+    //store the fruit objject into fruitTypes array[]
+    fruitTypes = [peach,watermelon];
+    sliceSound = loadSound('assets/fruit-ninja-combo.mp3');
+    backgroundTrack = loadSound('assets/fruit-ninja-bgtrack.mp3');
 }
-
 function setup(){
-    new Canvas(800, 600);
-    // set gravity to 10;
+    new Canvas(800,600);
+    //set gravity to 10
     world.gravity.y = 10;
-
-    fruitGroup = new Group();
+    fruitGroup= new Group();
     fruitHalves = new Group();
-    
-    // fruittype = [];
 }
-
 function draw(){
     clear();
-    image(dojoBG, 0, 0, width, height);
-
-    if((kb.presses(' ')||mouse.presses()) && (gameState === 'start')){
-        gameState = 'play';
-        score = 0;
-        fruitGroup.removeAll();
-        fruitHalves.removeAll();
+    image(dogoBG,0,0,width,height);
+    if((kb.presses(' ')||mouse.presses()) && (gameState==='start')){
+           gameState ='play';
+           score =0;
+           missFruits =0;
+           fruitGroup.removeAll();
+           fruitHalves.removeAll();
     }
-
     if(gameState === 'start'){
-        fill(0, 100);
-        rect(0, 0, width, height);
-        fill(225);
-        textAlign(CENTER, CENTER);
+        fill(0,180);
+        rect(0,0,width,height);
+        fill(255);
+        textAlign(CENTER,CENTER);
         textSize(48);
-        text("Fruit Ninja", width/2, height/2);
+        text('Fruit Ninja',width/2,height/2);
         textSize(24);
-        text("Press SPACE or Click to Start", width/2, height/2+30);
+        text('Press SPACE or Click to Start',width/2,height/2+30);
         return;
     }
-    if (frameCount % 60 === 0){// % refers to the remainder so if framecount divided by 120, if the remainder is 0, theb I will call spawnfruit function
+   
+    // % refers to the remainder so if framecount / by 120 and the remainder is 0 then i call spawnfruit function
+    if(frameCount % 120 === 0){
         spawnFruit();
     }
-    // handles my slicicng effect
     if(mouse.pressing()){
-        trail = new Sprite(mouse.x, mouse.y, 10);
+        trail = new Sprite(mouse.x,mouse.y,7);
         trail.collider = 'none';
-        trail.color = "PINK";
+        trail.color ="red";
         trail.life = 10;
         sliceFruit();
     }
-    //trying to access the items in the group
+    //trrying access the items in the group
     for(fruits of fruitGroup){
-        if(fruits.y > height + 50){
+        if(fruits.y> height +50){
             fruits.remove();
-            MissedFruits +=1;
-            
+            missFruits +=1;
         }
-        // if(MissedFruits ==3){
+        // if(missFruits==3){
         //     noLoop();
         // }
     }
-
-    stroke(158, 69, 69);
+    stroke(158,69,69);
     fill(255);
     textSize(24);
-    textAlign(LEFT, TOP);
-    text ("Score: " + score, 10, 10);
-    text ("Missed Fruits: " + MissedFruits, 200, 10);
-    if(!backgroundTrack.isPlaying()){
+    textAlign(LEFT,TOP);
+    text ('Score:' + score ,10,10);
+    text('Missed:' + missFruits,200,10);
+     if(!backgroundTrack.isPlaying()){
         backgroundTrack.loop();
     }
+}
 
+function spawnFruit(){
+    let fruitData = random(fruitTypes);
+    let randomX = random(300,500);
+    let fruit = new fruitGroup.Sprite(randomX,600+20,40); 
+    fruit.image = fruitData.whole;
+    fruit.type = fruitData;
+    fruit.vel.y = random(-10,-14);
+    fruit.vel.x = random(-2,2);
+    fruit.friction = 0;
 }
 
 function sliceFruit(){
@@ -99,61 +100,34 @@ function sliceFruit(){
         if(fruit.sliced){
             continue;
         }
-    
-
-        let d = dist(mouse.x, mouse.y, fruit.x, fruit.y);
-
-        if(d < (fruit.d/2) + 5){
-            fruit.sliced = true;
+        let distance  = dist(mouse.x,mouse.y,fruit.x,fruit.y);
+        if(distance<((fruit.diameter/2)+5)){
             const fx = fruit.x;
             const fy = fruit.y;
+            fruit.sliced= true;
             fruit.remove();
             sliceSound.play();
-            splitFruit(fx, fy, fruit.type);
+            splitFruit(fx,fy,fruit.type);
             score +=1;
             break;
         }
     }
 }
-
-function spawnFruit(){
-    // fruitTypes = [peach, watermelon];
-    // let peach = {
-    //     whole:loadImage('assets/peachwhole.png')
-    // };
-    //tell the program to randomly pick one fruit object
-    let fruitData = random(fruitTypes);
-    let randomX = random(300, 500);
-    // create a fruit and stroing it in the group
-    let fruit = new fruitGroup.Sprite(randomX, height+ 20, 40);
-    //basically loading the image adnt eh image is sored in the array and the key to the image values
-    //is whole hence its fruitData.whole (arrayname.whichkey)
-    fruit.image = fruitData.whole;
-    //telling the program that it is going to be a random fruit form the array
-    fruit.type = fruitData;
-    //setting the game physics the numbers can play around
-    fruit.vel.y = random(-10, - 14);
-    fruit.vel.x = random(-2, 2);
-    fruit.friction = 0;
-}
-
-function splitFruit(x, y, fruitData){
-    // left half 
-    let left = new fruitHalves.Sprite(x-10, y, 40, 40);
-    left.img = fruitData.half1;
+function splitFruit(x,y,fruitData){
+    let left = new fruitHalves.Sprite(x-10,y,40,40);
+    left.img = fruitData.half;
+    //set the physics
     left.vel.x = -3;
-    left.vel.y = random(-15, -2);
+    left.vel.y = random(-5,-2);
     left.rotationSpeed = -5;
     left.life = 30;
 
-
-
-    // right half
-    let right = new fruitHalves.Sprite(x+10, y, 40, 40);
-    right.img = fruitData.half1;
-    right.vel.x = -3;
-    right.vel.y = random(-5, -2);;
+    //do for the right as well
+    let right = new fruitHalves.Sprite(x-10,y,40,40);
+    right.img = fruitData.half;
+    //set the physics
+    right.vel.x = 3;
+    right.vel.y = random(-5,-2);
     right.rotationSpeed = 5;
     right.life = 30;
-
 }
