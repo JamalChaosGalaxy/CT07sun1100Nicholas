8042909b3ab2b04ad7f0188c9b1f3aca0bdb784f
JamalChaosGalaxy <nicholaswg1@gmail.com>
2025-10-12 03:38:09 +0000

    Oct 12, 2025, 3:38 AM

--- fruitninja/fruitninja04.js
+++ fruitninja/fruitninja04.js
@@ -35,7 +35,7 @@ function draw(){
     clear();
     image(dojoBG, 0, 0, width, height);
 
-    if(kb.presses(' ') )
+    if(kb.presses(' ')||mouse.presses()) 
 
     if(gameState === 'start'){
         fill(0, 100);
