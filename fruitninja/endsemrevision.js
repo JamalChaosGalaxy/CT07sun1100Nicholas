e47f0a766535c110725c846f00d0e9b8cb7310af
JamalChaosGalaxy <nicholaswg1@gmail.com>
2025-10-19 03:56:54 +0000

    Oct 19, 2025, 3:56 AM

---
+++ fruitninja/endsemrevision.js
@@ -0,0 +1,39 @@
+let square;
+let background;
+let squarethatcanfly;
+function preload(){
+    background = loadImage('assets/background-day.png');
+    squareImg = loadImage('assets/redbird-midflap.png');
+}
+
+function setup(){
+
+    new Canvas(500, 700);
+    world.gravity.y = 10;
+    square = new Sprite()
+    square.x = 50;
+    square.y = 50;
+    square.height = 10;
+    square.width = 10;
+    square.img = squareImg;
+    square.collider = "static";
+
+    squarethatcanfly = new Sprite()
+    squarethatcanfly.x = 150;
+    squarethatcanfly.y = 150;
+    squarethatcanfly.height = 10;
+    squarethatcanfly.width = 10;
+    squarethatcanfly.collider = "dynamic";
+    squarethatcanfly.bounciness = 10;
+}
+
+function draw(){
+    image(background, 0, 0, width, height);
+
+     
+    if(mouse.presses('') || kb.presses('')){
+        squarethatcanfly.vel.y = -5;
+        squarethatcanfly.sleeping = false;
+    }
+
+}
