322e77384bd5512fb42151cdabb5acc47a008e3e
JamalChaosGalaxy <nicholaswg1@gmail.com>
2025-10-19 04:01:21 +0000

    Oct 19, 2025, 4:01 AM

--- flappybird/playground/endsemrevision.js
+++ flappybird/playground/endsemrevision.js
@@ -8,30 +8,30 @@ function preload(){
 
 function setup(){
 
-    world.gravity.y = 10;
-    new Canvas(500, 700);
-    square = new Sprite()
-    square.x = 50;
-    square.y = 50;
-    square.height = 10;
-    square.width = 10;
-    square.img = squareImg;
-    square.collider = "static";
+    // world.gravity.y = 10;
+    // new Canvas(500, 700);
+    // square = new Sprite()
+    // square.x = 50;
+    // square.y = 50;
+    // square.height = 10;
+    // square.width = 10;
+    // square.img = squareImg;
+    // square.collider = "static";
 
-    squarethatcanfly = new Sprite()
-    squarethatcanfly.x = 150;
-    squarethatcanfly.y = 150;
-    squarethatcanfly.height = 10;
-    squarethatcanfly.width = 10;
-    squarethatcanfly.collider = "dynamic";
-    squarethatcanfly.bounciness = 10;
+    // squarethatcanfly = new Sprite()
+    // squarethatcanfly.x = 150;
+    // squarethatcanfly.y = 150;
+    // squarethatcanfly.height = 10;
+    // squarethatcanfly.width = 10;
+    // squarethatcanfly.collider = "dynamic";
+    // squarethatcanfly.bounciness = 10;
 }
 
 function draw(){
     image(background, 0, 0, width, height);
 
      
-    if(mouse.presses('') || kb.presses('')){
+    if(mouse.presses('') || kb.presses('space')){
         squarethatcanfly.vel.y = -5;
         squarethatcanfly.sleeping = false;
     }
