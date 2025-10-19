58de5eeb38727a64ed0709115c7a976c7611a942
JamalChaosGalaxy <nicholaswg1@gmail.com>
2025-10-19 04:00:07 +0000

    Oct 19, 2025, 4:00 AM

--- fruitninja/endsemrevision.js
+++ fruitninja/endsemrevision.js
@@ -11,29 +11,29 @@ function setup(){
     new Canvas(500, 700);
     world.gravity.y = 10;
     square = new Sprite(width/2, height/2, width, height);
-    square.x = 50;
-    square.y = 50;
-    square.height = 10;
-    square.width = 10;
-    square.img = squareImg;
-    square.collider = "static";
+//     square.x = 50;
+//     square.y = 50;
+//     square.height = 10;
+//     square.width = 10;
+//     square.img = squareImg;
+//     square.collider = "static";
 
-    squarethatcanfly = new Sprite()
-    squarethatcanfly.x = 150;
-    squarethatcanfly.y = 150;
-    squarethatcanfly.height = 10;
-    squarethatcanfly.width = 10;
-    squarethatcanfly.collider = "dynamic";
-    squarethatcanfly.bounciness = 10;
+//     squarethatcanfly = new Sprite()
+//     squarethatcanfly.x = 150;
+//     squarethatcanfly.y = 150;
+//     squarethatcanfly.height = 10;
+//     squarethatcanfly.width = 10;
+//     squarethatcanfly.collider = "dynamic";
+//     squarethatcanfly.bounciness = 10;
 }
 
 function draw(){
     image(background, 0, 0, width, height);
 
      
-    if(mouse.presses('') || kb.presses('')){
-        squarethatcanfly.vel.y = -5;
-        squarethatcanfly.sleeping = false;
+    if(mouse.presses('') || kb.presses('space')){
+        square.vel.y = -5;
+        square.sleeping = false;
     }
 
 }
