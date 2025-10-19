78c64d8ccdd240e0ba3b76009242c12dba65a0c6
JamalChaosGalaxy <nicholaswg1@gmail.com>
2025-10-19 03:19:12 +0000

    Oct 19, 2025, 3:19 AM

--- fruitninja/endsemrevision.js
+++ fruitninja/endsemrevision.js
@@ -5,7 +5,9 @@ function preload(){
 function setup(){
     new Canvas(500, 700);
     square = new Sprite()
-    square.x = 50
+    square.x = 50;
+    square.y = 50;
+    square.height = 
 }
 
 function draw(){
