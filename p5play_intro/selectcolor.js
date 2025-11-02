010a4071787199be6d40e06d668a2de9a9173f2e
JamalChaosGalaxy <nicholaswg1@gmail.com>
2025-11-02 03:53:15 +0000

    Nov 2, 2025, 3:53 AM

--- p5play_intro/selectcolor.js
+++ p5play_intro/selectcolor.js
@@ -8,7 +8,7 @@ let colorpicker;
 function setup(){
     createCanvas(600, 400);
     //create colour picker
-    colorPick = createColorPi
+    colorPick = createColorPicker("p")
     // create Text box to use
     inputText = createInput();
     inputText.position(690, 450);
