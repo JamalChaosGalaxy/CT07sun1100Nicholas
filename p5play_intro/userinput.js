b1656c4408c3adf0eab4293dde703a17df317794
JamalChaosGalaxy <nicholaswg1@gmail.com>
2025-11-02 03:47:33 +0000

    Nov 2, 2025, 3:47 AM

--- p5play_intro/userinput.js
+++ p5play_intro/userinput.js
@@ -24,8 +24,8 @@ function draw(){
     text(userName, width/2, 180);
     text(userAge, width/2, 210);
     textSize(10);
-    text(textone, 360, 450);
-    text(texttwo, 360, 470);
+    text(textone, 360, 250);
+    text(texttwo, 360, 270);
 }
 
 function updateText(){
