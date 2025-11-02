25b1b9d7ddf33aa70c61db49ff3d196478c9cb31
JamalChaosGalaxy <nicholaswg1@gmail.com>
2025-11-02 03:25:00 +0000

    Nov 2, 2025, 3:25 AM

--- p5play_intro/userinput.js
+++ p5play_intro/userinput.js
@@ -4,7 +4,7 @@ function setup(){
     createCanvas(600, 400);
     // create Text box to use
     inputText = createInput();
-    inputText.position(width/2, height/2);
+    inputText.position(width/2, 180);
     inputText.input(updateText);
 }
 
